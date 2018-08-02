bf875a91003e47a3f8bf66b8ec71fc348976e69a
kthngx <kthngx@users.noreply.github.com>
2018-08-02 13:26:42 +1000

    Daily coding

--- 19_Analyse_combined_region.py
+++ 19_Analyse_combined_region.py
@@ -54,7 +54,7 @@ df_reindexed.reset_index(inplace = True)
 def calcdistance_km(latA,lonA,latB,lonB):
     dist = np.sqrt(np.square(latA-latB)+np.square(lonA-lonB))*111
     return np.int(dist)
-
+#    return True
 #%% Create a HDF5 file
 Hfile_label = h5py.File('TCDORIAN_label.h5','w')
 #Hfile_label.close()
@@ -74,6 +74,143 @@ Hfile_imag.close()
 Hfile_label.close()
 
 
+
+
+#%% OPEN HDF5 FILES
+Hfile_imag = h5py.File('TCDORIAN.h5','r')
+Hfile_label = h5py.File('TCDORIAN_label.h5','r+')  
+
+#%% CLOSE HDF5 FILES
+Hfile_imag.close()
+Hfile_label.close()
+          
+
+#%% Run full algorithm for the whole first image
+#%%  core detection         
+C_label_TC = Hfile_label['label_TC']
+# load the scene
+Clat = Hfile_imag['latitude'][:]
+Clon = Hfile_imag['longitude'][:]
+imag_px_w = np.shape(Clon)[0]
+imag_px_h = np.shape(Clat)[0]
+imag_res = 4 #km
+
+# load all file with '2013*'
+keys = Hfile_imag.keys()
+files = [key for key in keys if key.startswith('2013')]
+
+# first image
+C_i = 0
+C_label_TC[:,:,C_i] = np.zeros([dim_lat,dim_lon])
+filename = files[C_i]
+C_btemp = Hfile_imag[filename][:]
+C_flag = C_label_TC[:,:,C_i][:]
+
+# load best track centre
+BT_cen = df_reindexed.iloc[C_i]
+BT_lat = BT_cen.lat
+BT_lon = BT_cen.lon
+
+# define search boundry
+S_bound_km = 300 #km
+S_bound_deg = S_bound_km/111 #convert km to deg
+S_no_px = np.round(S_bound_km/imag_res)
+
+# 
+box_i_w = [i_w for i_w,x_w in enumerate(Clon) if abs(BT_lon-x_w) < S_bound_deg]
+box_i_h = [i_h for i_h,x_h in enumerate(Clat) if abs(BT_lat-x_h) < S_bound_deg]
+
+#
+for i_w in box_i_w:
+    for i_h in box_i_h:
+        t_lat = Clat[i_h]
+        t_lon = Clon[i_w]
+        t_btemp = C_btemp[i_h,i_w]
+        if (calcdistance_km(BT_lat, BT_lon, t_lat, t_lon) < S_bound_km) and (np.int(t_btemp)) < 200:
+            C_flag[i_h,i_w] = 1
+            print ('found at ' + str(i_w) + ' and ' + str(i_h))
+#%% spreading the core
+Tb_thres = 280
+start_time_overall = time.time()
+
+S_bound_tot_km = 1110 #km
+S_bound_tot_deg = S_bound_tot_km/111 #convert km to deg
+S_no_size_px = np.round(S_bound_tot_km/imag_res)
+
+stop_flag = 0
+iteration = 1
+while stop_flag == 0:
+    start_time_itr = time.time()
+    stop_flag = 1
+    idx_flag = np.where(C_flag==1)
+    
+    for i in range(0,np.shape(idx_flag)[1]-1):
+            idx_h = idx_flag[0][i]
+            idx_w = idx_flag[1][i]
+            C_label_TC[idx_h,idx_w,C_i] = 2
+            for jy in range (0,5):
+                for jx in range (0,5):
+                    idx_hj = idx_h + jy
+                    idx_wj = idx_w + jx
+                    if (calcdistance_km(BT_lat, BT_lon, Clat[idx_h], Clon[idx_w]) < S_bound_tot_km) and (C_btemp[idx_hj,idx_wj]<=Tb_thres) and (C_flag[idx_hj,idx_wj]==0):
+                            C_flag[idx_hj,idx_wj]=1
+                            stop_flag = 0
+                    
+                    idx_hj = idx_h + jy
+                    idx_wj = idx_w - jx
+                    if (calcdistance_km(BT_lat, BT_lon, Clat[idx_h], Clon[idx_w]) < S_bound_tot_km) and (C_btemp[idx_hj,idx_wj]<=Tb_thres) and (C_flag[idx_hj,idx_wj]==0):
+                            C_flag[idx_hj,idx_wj]=1
+                            stop_flag = 0
+                    
+                    idx_hj = idx_h - jy
+                    idx_wj = idx_w + jx
+                    if (calcdistance_km(BT_lat, BT_lon, Clat[idx_h], Clon[idx_w]) < S_bound_tot_km) and (C_btemp[idx_hj,idx_wj]<=Tb_thres) and (C_flag[idx_hj,idx_wj]==0):
+                            C_flag[idx_hj,idx_wj]=1
+                            stop_flag = 0      
+                    
+                    idx_hj = idx_h - jy
+                    idx_wj = idx_w - jx
+                    if (calcdistance_km(BT_lat, BT_lon, Clat[idx_h], Clon[idx_w]) < S_bound_tot_km) and (C_btemp[idx_hj,idx_wj]<=Tb_thres) and (C_flag[idx_hj,idx_wj]==0):
+                            C_flag[idx_hj,idx_wj]=1
+                            stop_flag = 0
+                    
+    print ("stop flag value: "+ str(stop_flag))
+    elapsed_time_itr = time.time() - start_time_itr
+    print ('Layer ' + str(C_i) + ' Interation ' + str(iteration) + ' done in ' +  time.strftime("%H:%M:%S", time.gmtime(elapsed_time_itr)))
+    iteration = iteration + 1
+C_label_TC[:,:,C_i] = C_flag    
+        
+
+#%% Plot image
+c_flag = C_label_TC[:,:,C_i][:]
+flag_pos = np.where(c_flag==1)
+c_mask = np.where(c_flag == 0, np.NaN , c_flag)
+mask_pos = np.where(c_mask>0)
+#c_Tb = Cdataset.Tb[C_i,:,:].values
+
+#%% plot IR image and the center point
+fig = plt.figure()
+lat_max = np.round(np.max(Clat),1)
+lat_min = np.round(np.min(Clat),1)
+lon_max = np.round(np.max(Clon),1)
+lon_min = np.round(np.min(Clon),1)
+
+im = plt.imshow(C_btemp, extent = (lon_min, lon_max, lat_min, lat_max),  cmap='Greys',origin='lower')
+mycmap = colors.ListedColormap(['yellow'])
+im2 = plt.imshow(c_mask, extent = (lon_min, lon_max, lat_min, lat_max), cmap=mycmap,origin='lower',alpha=0.4)
+cb = fig.colorbar(im, orientation='vertical')
+cb.set_label('Brightness Temperature(K)')
+ax = plt.gca()
+ax.set_title('TC Dorina    '+files[C_i].replace(".nc",""))
+ax.set_xlabel('Longitude')
+ax.set_ylabel('Latitude')
+#plt.plot(BT_lon,BT_lat,'or', markersize = 2)            
+
+
+
+
+################################################################################################
+#%%           TEST SITE
 
 #%% Run full algorithm for the whole first image
 Hfile_imag = h5py.File('TCDORIAN.h5','r')
@@ -225,13 +362,7 @@ ax = plt.gca()
 ax.set_title('TC Dorina    '+files[C_i].replace(".nc",""))
 ax.set_xlabel('Longitude')
 ax.set_ylabel('Latitude')
-#plt.plot(BT_lon,BT_lat,'or', markersize = 2)            
-
-
-
-
-################################################################################################
-#%%           TEST SITE
+#plt.plot(BT_lon,BT_lat,'or', markersize = 2)           
 
 
 #%% Run the algorithm for 1 image, Run to only detect the core  
